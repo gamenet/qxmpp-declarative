5b6ea751037d19441451cc43d6f38ef45e205e70
Nikita Gorbunov <nikita.gorbunov@syncopate.ru>
2015-03-12 11:07:15 +0300

    Added filtration for carbons, added explicit from field (full jid) when sending message

--- qxmpp-declarative/src/QmlQXmppClient.cpp
+++ qxmpp-declarative/src/QmlQXmppClient.cpp
@@ -173,6 +173,8 @@ void QmlQXmppClient::sendMessage(const QString& bareJid, QVariantMap map)
 
   if (map.contains(QString("attentionRequest")))
     msg.setAttentionRequested(map["attentionRequest"].toBool());
+
+  msg.setFrom(this->_client.configuration().jid());
   
   QStringList resources = this->_client.rosterManager().getResources(bareJid);
   if (!resources.isEmpty()) {
@@ -228,8 +230,11 @@ void QmlQXmppClient::onError(QXmppClient::Error code)
 void QmlQXmppClient::onMessageReceived(const QXmppMessage& message)
 {
   if (message.hasMessageCarbon()) {
-    QmlQXmppMessage qmlmessage(message.carbonMessage());
-    emit carbonMessageReceived(&qmlmessage);
+    QString myFullJid = this->_client.configuration().jid();
+    if (myFullJid != message.carbonMessage().from()) {
+      QmlQXmppMessage qmlmessage(message.carbonMessage());
+      emit carbonMessageReceived(&qmlmessage);
+    }
     return;
   }
 
